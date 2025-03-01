808b8504ca0b8ae456cb9fa851aaba35d978cffa
Sharanya Devi <141646902+sharanya29devi@users.noreply.github.com>
2025-03-01 21:55:27 +0530

    p2.py

---
+++ p2.py
@@ -0,0 +1,8 @@
+inte1 =int(input("Enter the i1: "))
+inte2 =int(input("Enter the i2: "))
+inte3 =int(input("Enter the i3: "))
+
+if ((inte1 + inte2 + inte3) == 180):
+    print("Triangle can be formed")
+else:
+    print("Triangle cannot be formed")
