27703ec6d92e09f7d2cda8a527a409735b7404ce
Sharanya Devi <141646902+sharanya29devi@users.noreply.github.com>
2025-03-01 22:12:49 +0530

    p3.py

---
+++ p3.py
@@ -0,0 +1,12 @@
+a =int(input("Enter the integer: "))
+
+if (a >= 90):
+    print("Grade A")
+elif (a>=80):
+    print("Grade B")
+elif (a>=60):
+    print("Grade C")
+elif (a>=35):
+    print("Grade D")
+else:
+    print("Fail")
