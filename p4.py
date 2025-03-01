16f924ec8a62df636bbdb43fcf37b8fdd1d2c2ba
Sharanya Devi <141646902+sharanya29devi@users.noreply.github.com>
2025-03-01 22:45:25 +0530

    p4.py

---
+++ p4.py
@@ -0,0 +1,12 @@
+a =int(input("Enter the integer: "))
+
+if (a == 29):
+    print("Small")
+elif (a == 30):
+    print("Medium")
+elif (a == 38):
+    print("Large")
+elif (a == 42):
+    print("XLarge")    
+else:
+    print("Invalid")
